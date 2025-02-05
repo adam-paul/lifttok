dcded537f4cc44f4c989eb741557aed0ed32ee2f
Adam Paul <notadampaul@protonmail.com>
2025-02-05 09:27:33 -0600

    adding landing page button icons

--- App.js
+++ App.js
@@ -2,6 +2,7 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
 
 import FeedScreen from './src/screens/FeedScreen';
 import RecordScreen from './src/screens/RecordScreen';
@@ -12,10 +13,42 @@ const Tab = createBottomTabNavigator();
 export default function App() {
   return (
     <NavigationContainer>
-      <Tab.Navigator>
-        <Tab.Screen name="Feed" component={FeedScreen} />
-        <Tab.Screen name="Record" component={RecordScreen} />
-        <Tab.Screen name="Profile" component={ProfileScreen} />
+      <Tab.Navigator
+        screenOptions={{
+          tabBarStyle: { backgroundColor: '#000' },
+          tabBarActiveTintColor: '#ff4040',
+          tabBarInactiveTintColor: '#666',
+          headerStyle: { backgroundColor: '#000' },
+          headerTintColor: '#fff'
+        }}
+      >
+        <Tab.Screen 
+          name="Feed" 
+          component={FeedScreen}
+          options={{
+            tabBarIcon: ({ color, size }) => (
+              <MaterialCommunityIcons name="ticket-confirmation-outline" size={size} color={color} />
+            ),
+          }}
+        />
+        <Tab.Screen 
+          name="Record" 
+          component={RecordScreen}
+          options={{
+            tabBarIcon: ({ color, size }) => (
+              <MaterialCommunityIcons name="record-circle-outline" size={size} color={color} />
+            ),
+          }}
+        />
+        <Tab.Screen 
+          name="Profile" 
+          component={ProfileScreen}
+          options={{
+            tabBarIcon: ({ color, size }) => (
+              <MaterialCommunityIcons name="account-outline" size={size} color={color} />
+            ),
+          }}
+        />
       </Tab.Navigator>
     </NavigationContainer>
   );
